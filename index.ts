3c9a9d66b0ff624a74fb3ff8271e50dbde3c5758
Doğa Budak <dogabudak@gmail.com>
2021-11-03 15:49:08 +0300

    added elastic search transporter for the logger

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
 import * as winston from "winston";
+import {ElasticsearchTransport} from "winston-elasticsearch";
+
+const esTransportOpts = {
+    level: 'info'
+};
+const esTransport = new ElasticsearchTransport(esTransportOpts);
+esTransport.on('error', (error) => {
+    console.error('Error in logger caught', error);
+});
 export const logger = winston.createLogger({
     format: winston.format.json(),
     transports: [
         new winston.transports.Console(),
+        esTransport,
         new winston.transports.File({ filename: 'combined.log' })
     ]
 });
